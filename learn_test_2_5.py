9a974f3c97913a18c5de2832e1ac1fdd3a5f1426
nikdie42 <80208382+nikdie42@users.noreply.github.com>
2021-03-21 19:11:10 +0100

    Add files via upload

--- learn_test_2_5.py
+++ learn_test_2_5.py
@@ -14,10 +14,36 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras.utils import to_categorical
 
 # laden der aufgeteilten Kategorien
-Testset = np.load('TestsetCifar10.npy')
-Trainingsset = np.load('TrainingssetCifar10.npy')
+#Testset = np.load('TestsetCifar10.npy')
+#Trainingsset = np.load('TrainingssetCifar10.npy')
 yTest = np.load('yTestset.npy')
 yTraining = np.load('yTrainingsset.npy')
+cl0 = np.load('cl0.npy')
+cl1 = np.load('cl1.npy')
+cl2 = np.load('cl2.npy')
+cl3 = np.load('cl3.npy')
+cl4 = np.load('cl4.npy')
+cl5 = np.load('cl5.npy')
+cl6 = np.load('cl6.npy')
+cl7 = np.load('cl7.npy')
+cl8 = np.load('cl8.npy')
+cl9 = np.load('cl9.npy')
+
+Testset = []
+Testset.append(cl5)
+Testset.append(cl6)
+Testset.append(cl7)
+Testset.append(cl8)
+Testset.append(cl9)
+Trainingsset = []
+Trainingsset.append(cl0)
+Trainingsset.append(cl1)
+Trainingsset.append(cl2)
+Trainingsset.append(cl3)
+Trainingsset.append(cl4)
+
+Testset = np.array(Testset)
+Trainingsset = np.array(Trainingsset)
 data=Testset[2][3]
 
 # Erstellen eines zufälligen Test- und Trainings Subset
@@ -57,36 +83,50 @@ def getnerate_random_dataset(dataset,trainsize,testsize, K, datasize):
     trainx=[]
     trainy=[]
     trainpicturenumbers=[]
+    trainselection=[]
+
     testx=[]
     testy=[]
     testpicturenumbers=[]
-    
+    testselection=[]
+
+    trainnumber=0
+    testnnumber=0
+    weightsum=0
+
+    for picturetype in dataset:
+        for picture in picturetype:
+            weightsum += picture[2]
     for i in range(trainsize):
-        compare_weight=20
-        picturnumber = 0
-        picturtype = 0
-        while 1:
-            picturtype=random.randrange(5)
-            picturnumber=random.randrange(datasize)
-            compare_weight=random.uniform(0, 1)
-            if compare_weight <= dataset[picturtype][picturnumber][2] * 1000:
-                trainx.append(dataset[picturtype][picturnumber][0])
-                trainy.append(dataset[picturtype][picturnumber][1])
-                trainpicturenumbers.append([picturtype,picturnumber])
-                break
+        trainselection.append(random.uniform(0, weightsum))
+    trainselection.append(weightsum+1)
     for i in range(testsize):
-        compare_weight=20
-        picturnumber = 0
-        picturtype = 0
-        while 1:
-            picturtype=random.randrange(5)
-            picturnumber=random.randrange(datasize)
-            compare_weight=random.uniform(0, 1)
-            if compare_weight <= dataset[picturtype][picturnumber][2] * 1000:
-                testx.append(dataset[picturtype][picturnumber][0])
-                testy.append(dataset[picturtype][picturnumber][1])
-                testpicturenumbers.append([picturtype,picturnumber])
-                break
+        testselection.append(random.uniform(0, weightsum))
+    testselection.append(weightsum+1)
+    trainselection=np.asarray(trainselection)
+    trainselection=np.sort(trainselection)
+    testselection=np.asarray(testselection)
+    testselection=np.sort(testselection)
+    print(weightsum)
+    weightsum=0
+    for picturetype in range(len(dataset)):
+        for picturnumber in range(len(dataset[picturetype])):
+            print(weightsum)
+            weightsum += dataset[picturetype][picturnumber][2]
+            if weightsum>trainselection[trainnumber]:
+                trainpicturenumbers.append([picturetype,picturnumber])
+                trainx.append(dataset[picturetype][picturnumber][0])
+                trainy.append(dataset[picturetype][picturnumber][1])
+                trainnumber += 1
+
+            elif weightsum>testselection[testnnumber]:
+                testpicturenumbers.append([picturetype,picturnumber])
+                testx.append(dataset[picturetype][picturnumber][0])
+                testy.append(dataset[picturetype][picturnumber][1])
+                testnnumber +=1
+
+    trainpicturenumbers = np.array(trainpicturenumbers)
+    testpicturenumbers = np.array(testpicturenumbers)
     trainx = np.array(trainx)
     trainy = np.array(trainy)
     trainy = to_categorical(trainy,5)
@@ -94,10 +134,6 @@ def getnerate_random_dataset(dataset,trainsize,testsize, K, datasize):
     testy = np.array(testy)
     testyvergleich = testy
     testy = to_categorical(testy,5)
-    #print(np.shape(trainx))
-    #print(np.shape(trainy))
-    #print(np.shape(testx))
-    #print(np.shape(testy))
     return trainx, trainy, trainpicturenumbers, testx, testy, testpicturenumbers, testyvergleich
 
 def cnn_learn(trainx, trainy, testx, testy, testyvergleich):
@@ -120,17 +156,39 @@ def cnn_learn(trainx, trainy, testx, testy, testyvergleich):
     predy = CNN.predict(testx)
     choise = np.argmax(predy, axis=1)
     predy2 =np.argmax(predy,axis=1)
+    
+    print("predy2")
+    print(predy2)
 
     y = predy2 - testyvergleich
+    
     fehlermatrix = []
+    
     for i in range(len(testyvergleich)):
         if y[i] != 0:
             fehlermatrix.append(0)
         else:
             fehlermatrix.append(1)
-    #print("Fehlermatrix:")
-    #print(fehlermatrix)
-    return fehlermatrix
+            
+    klassen = [0,0,0,0,0]
+    for j in range(len(testyvergleich)):
+        if predy2[j] == 0 and fehlermatrix[j] == 1:
+            klassen[0] = klassen[0] + 1
+        if predy2[j] == 1 and fehlermatrix[j] == 1:
+            klassen[1] = klassen[1] + 1
+        if predy2[j] == 2 and fehlermatrix[j] == 1:
+            klassen[2] = klassen[2] + 1    
+        if predy2[j] == 3 and fehlermatrix[j] == 1:
+            klassen[3] = klassen[3] + 1
+        if predy2[j] == 4 and fehlermatrix[j] == 1:
+            klassen[4] = klassen[4] + 1
+                    
+    klassen = np.array(klassen)
+    besteKlasse = klassen[np.argmax(klassen)]
+    klasse = np.argmax(klassen)
+   
+            
+    return fehlermatrix, besteKlasse, klasse
 
 def calc_error(fehlermatrix, testnpicturenumbers, dataset):
     i=0
@@ -141,6 +199,26 @@ def calc_error(fehlermatrix, testnpicturenumbers, dataset):
     print("Fehler:")
     print(error)
     return error
+
+def calc_besteBilder(anz_bilder_klasse):
+    #print(anz_bilder_klasse)
+    
+    summen = [0,0,0,0,0]
+    for i in range(len(anz_bilder_klasse)):
+        if anz_bilder_klasse[i][1] == 0:
+            summen[0] = summen[0] + anz_bilder_klasse[i][0]
+        if anz_bilder_klasse[i][1] == 1:
+            summen[1] = summen[1] + anz_bilder_klasse[i][0]
+        if anz_bilder_klasse[i][1] == 2:
+            summen[2] = summen[2] + anz_bilder_klasse[i][0]
+        if anz_bilder_klasse[i][1] == 3:
+            summen[3] = summen[3] + anz_bilder_klasse[i][0]
+        if anz_bilder_klasse[i][1] == 4:
+            summen[4] = summen[4] + anz_bilder_klasse[i][0]
+    summen = np.array(summen)
+    maxBilder = summen[np.argmax(summen)]
+    return maxBilder
+            
 
 def calc_sum(fehlermatrix, testnpicturenumbers, dataset,beta):
     summe = 0
@@ -170,8 +248,8 @@ def calc_wheigths(fehlermatrix, testnpicturenumbers, dataset, beta):
 """Learn++"""
 
 def learn_plusplus(Trainingsset, K, T): 
-    beta_t = []
-    H_t = []
+    anz_bilder_klasse = []
+    B_t =  []
     newdata=addy(Trainingsset)
     split_data=split_dataset(K,newdata)
     m = len(split_data[0])
@@ -184,6 +262,8 @@ def learn_plusplus(Trainingsset, K, T):
       
         # Zählschleife mit Anzahl der Iterationen
         t = 0
+        beta_t = []
+        H_t = []
         while(t < T):  
                 print("T:")
                 print(t)
@@ -194,8 +274,10 @@ def learn_plusplus(Trainingsset, K, T):
                 # call weak learn / neural network mit zufälligen Trainings- und Testsubset
                 
                 
-                fehlermatrix = cnn_learn(trainx, trainy, testx, testy, testyvergleich)
+                fehlermatrix, bilderBesteKlasse, besteKlasse = cnn_learn(trainx, trainy, testx, testy, testyvergleich)
                 epsilon = calc_error(fehlermatrix, testpicturenumbers, split_data[k])
+                bilder_klasse = [bilderBesteKlasse, besteKlasse]
+                anz_bilder_klasse.append(bilder_klasse)
                 
                 
                 if epsilon <= 0.125:
@@ -211,26 +293,34 @@ def learn_plusplus(Trainingsset, K, T):
                         
                     beta_t1 = np.array(beta_t)
                     summe = np.sum(beta_t1)
-                    Ht = summe
+                    Ht = summe * bilderBesteKlasse
                     H_t.append(summe)
                     print("Ht:")
                     print(Ht)
                     
                     Et = calc_errorE(fehlermatrix, testpicturenumbers, split_data[k])
                     
-                    if Et <= (testsize/m)*0.5:
+                    if Et <= 0.125:
                         Bt = Et/(1-Et)
+                        B_t.append(Bt)
                     
                     
                         calc_wheigths(fehlermatrix, testpicturenumbers, split_data[k], Bt)
+                        bilder_klasse = [bilderBesteKlasse, besteKlasse]
+                        anz_bilder_klasse.append(bilder_klasse)
                         t = t + 1
-        Hfinal = K * np.sum(H_t)
-        print("Hfinal:")
-        print(Hfinal)
+       
+    B_t = np.array(B_t)
+    summe_B = np.sum(B_t)
+    besteBilder = calc_besteBilder(anz_bilder_klasse)
+    print(besteBilder)
+    Hfinal = summe_B * besteBilder
+    print("Hfinal:")
+    print(Hfinal)
 
 """ MAIN SCRIPT ============================================================="""
 if __name__ == '__main__':
-    K=2
-    T=2
+    K=5
+    T=3
     learn_plusplus(Trainingsset, K, T)
 
